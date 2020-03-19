b295f4fc7824bf5be630b3407b52751c2fe27645
Hagar <hagar.aboelfottouh1@vodafone.com>
2020-03-19 12:50:11 +0200

    hashing the PW

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,21 +1,25 @@
 var express = require("express");
 var router = express.Router();
 var { User } = require("../models/user.model");
+//const bcrypt = require("bcrypt");
+var bcrypt = require('bcryptjs');
 
 
 //Get all
 router.get("/", async (req, res) => {
     let users = await User.find(function (error, response) {
         if (error) {
-            res.send(error);
+            res.status(400).send(error);
         } else {
-            res.send(response);
+            res.status(200).send(response);
         }
     });
 });
 
 //Add
 router.post("/add", async (req, res) => {
+
+    console.log("add user called >>>>> ");
     if (req.body) {
         var data = req.body;
     } else {
@@ -32,8 +36,20 @@ router.post("/add", async (req, res) => {
         } else {
             user.id = latestUser[0].id + 1;
         }
+        ///////////////////// 1 //////////////////////
+        // await bcrypt.hash(data.password, 10, async function (err, hash) {
+        //     user.password = hash;
+        //     console.log(hash);
+        // });
+        ///////////////////// 2 //////////////////////
+        // var hash = bcrypt.hash(data.password, 10);
+        // console.log(hash);
+
+
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(data.password, salt);
+        user.password = hash;
         user.email = data.email;
-        user.password = data.password;
 
         const result = await user.save();
         if (result) {
@@ -50,22 +66,28 @@ router.post("/add", async (req, res) => {
 router.put("/update", async (req, res) => {
     if (req.body) {
         var data = req.body;
+
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(data.password, salt);
+        try {
+            const filter = { _id: data.id };
+            const update = { email: data.email, password: hash };
+            let userResponse = await User.findOneAndUpdate(filter, update, {
+                new: true
+            });
+            console.log("response -> ", userResponse)
+            res.status(200).send({
+                message: "User Updated Successfully"
+            });
+        } catch (error) { }
+
     } else {
         return res.status(404).send("Request parameters are null");
     }
 
-    try {
 
-        var user = await User.find({ email: data.email });
-        if (!this.user) {
-            return res.status(404).send({ message: 'no record found' });
-        }
-        else {
-            user.email = data.email;
-            user.password = data.password;
-            const result = await user.save();
+});
 
-        }
 router.put("/changePassword", async (req, res) => {
     if (req.body) {
         var data = req.body;
